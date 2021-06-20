b9213f9ff8385d5fa6a12903778dc30ca898a06e
srinivas-kv <srinivaskv88841@gmail.com>
2021-06-20 19:37:48 +0530

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo _instance = new StudentInfo(); //Instance / space
+            _instance.setId("$100");
+            _instance.setName("Tom");
+            _instance.setMarks(60);
+            Console.WriteLine(_instance.getId());
+            Console.WriteLine(_instance.getName());
+            Console.WriteLine(_instance.getMarks());
+
+        }
+    }
+}
---
+++ Demo/StudentInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+#region User Defined Data Type
+    class StudentInfo
+    {
+        #region State - private
+        private string id;
+        private string name;
+        private int marks;
+        #endregion
+        #region Behaviors, public interfaces / Mutators for message passing
+        public string getId()
+        { return this.id; }
+        public string getName()
+        { return this.name; }
+        public int getMarks()
+        { return this.marks; }
+        public void setId(string value)
+        { this.id = value; }
+        public void setName(string value)
+        { this.name = value; }
+        public void setMarks(int value)
+        { this.marks = value; }
+        #endregion
+#endregion
+
+    }
+}
